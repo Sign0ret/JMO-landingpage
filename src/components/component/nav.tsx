2d0bb78d0ff30274abd31c15a296722c9af57c00
Sign0ret <adolfo.h.signoret@gmail.com>
2024-01-05 14:14:25 -0600

    initial commit landing page

---
+++ src/components/component/nav.tsx
@@ -0,0 +1,62 @@
+export const Nav = () => {
+
+  return (
+    <main key="1" className="flex flex-col w-full min-h-screen p-4 md:p-8 bg-[rgb(20,20,20)] text-[rgb(255,255,255)]">
+      <nav className="fixed left-80 right-80 mx-auto w-20% top-4 bg-[rgba(20,20,20,0.6)] z-10">
+        <div className="flex justify-center p-4">
+          <p className="mx-4 hover:pointer cursor-pointer">JMO arquitectos</p>
+          <p className="mx-4 hover:pointer cursor-pointer">Nosotros</p>
+          <p className="mx-4 hover:pointer cursor-pointer text-lg">Casas</p>
+          <p className="mx-4 hover:pointer cursor-pointer text-lg">Contacto</p>
+        </div>
+      </nav>
+      {/* Slide 1 */}
+      <div id="slide1" className={`h-screen bg-red-500  inset-0 w-screen}`}>
+        <img src="next.svg" alt="Slide 1"></img>
+      </div>
+      {/* Slide 2 */}
+      // Slide 2
+      <div id="slide2" className="relative h-screen w-screen overflow-x-hidden" >
+        <img
+          src="skeetch.jpg"
+          alt="Slide 2"
+          className="object-cover w-full h-full overflow-hidden"
+          style={{ position: 'absolute', top: 0, left: 0 }}
+        />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+          <div className="bg-[rgba(255,255,255,0.2)] p-8 text-black">
+            <h1 className="text-4xl font-bold">Your Text Here</h1>
+            <p className="mt-4 text-lg">Additional description or content</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Slide 3 */}
+      <div id="slide3" className={`h-screen bg-green-500  inset-0 w-screen}`}>
+        Slide 3 Content
+      </div>
+    </main>
+  );
+};
+
+  
+  
+  function HomeIcon(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+        <polyline points="9 22 9 12 15 12 15 22" />
+      </svg>
+    )
+  }
