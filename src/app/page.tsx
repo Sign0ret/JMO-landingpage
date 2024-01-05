c8f767f0711090f859e4ca1c3082667138abb41b
Sign0ret <adolfo.h.signoret@gmail.com>
2024-01-05 14:35:04 -0600

    initial commit for production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
-import { ConstructionBoard } from '@/components/component/construction-board'
 import { Nav} from '@/components/component/nav'
 export default function Home() {
   return (
       <Nav />
-      // <ConstructionBoard />
   )
 }
--- src/components/component/nav.tsx
+++ src/components/component/nav.tsx
@@ -15,7 +15,7 @@ export const Nav = () => {
         <img src="next.svg" alt="Slide 1"></img>
       </div>
       {/* Slide 2 */}
-      // Slide 2
+      {/* // Slide 2 */}
       <div id="slide2" className="relative h-screen w-screen overflow-x-hidden" >
         <img
           src="skeetch.jpg"
@@ -38,25 +38,3 @@ export const Nav = () => {
     </main>
   );
 };
-
-  
-  
-  function HomeIcon(props) {
-    return (
-      <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-        <polyline points="9 22 9 12 15 12 15 22" />
-      </svg>
-    )
-  }
