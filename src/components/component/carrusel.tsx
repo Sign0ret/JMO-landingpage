e56747f417e57320d0b6b6e7e1d2412c4a904bc5
Jorge Blásquez <a01637706@tec.mx>
2024-02-05 16:42:29 -0600

    Carrucel v1.1

--- src/components/component/carrusel.tsx
+++ src/components/component/carrusel.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css"
+import "./carrusel.css"
 
 export default function Carrusel() {
     const images = [
@@ -10,18 +11,40 @@ export default function Carrusel() {
             thumbnail: "https://dr.savee-cdn.com/things/thumbnails/6/2/0eb5985f8267ceabe9a3db.webp"
         },
         {
-            original: "https://dr.savee-cdn.com/things/thumbnails/6/5/33bd9df8aadf0116ebef0e.webp",
-            thumbnail: "https://dr.savee-cdn.com/things/thumbnails/6/5/33bd9df8aadf0116ebef0e.webp"
+            original: "https://dr.savee-cdn.com/things/thumbnails/5/c/25464646d7ca1fd955e6ff.webp",
+            thumbnail: "https://dr.savee-cdn.com/things/thumbnails/5/c/25464646d7ca1fd955e6ff.webp"
         },
         {
             original: "https://dr.savee-cdn.com/things/thumbnails/5/c/9411987c82ce7ab878bdd1.webp",
             thumbnail: "https://dr.savee-cdn.com/things/thumbnails/5/c/9411987c82ce7ab878bdd1.webp"
         },
+        {
+            original: "https://dr.savee-cdn.com/things/thumbnails/6/4/80c3c71b258af5203e9076.webp",
+            thumbnail: "https://dr.savee-cdn.com/things/thumbnails/6/4/80c3c71b258af5203e9076.webp"
+        },
+        {
+            original: "https://dr.savee-cdn.com/things/thumbnails/6/2/0a9d2cdbb3227de445fb0a.webp",
+            thumbnail: "https://dr.savee-cdn.com/things/thumbnails/6/2/0a9d2cdbb3227de445fb0a.webp"
+        },
+        {
+            original: "https://dr.savee-cdn.com/things/thumbnails/5/b/46852e3e82354d17300112.webp",
+            thumbnail: "https://dr.savee-cdn.com/things/thumbnails/5/b/46852e3e82354d17300112.webp"
+        },
+        {
+            original: "https://dr.savee-cdn.com/things/thumbnails/5/b/ec79adfff57a530b4de672.webp",
+            thumbnail: "https://dr.savee-cdn.com/things/thumbnails/5/b/ec79adfff57a530b4de672.webp"
+        },
     ];
 
   return (
-    <div>
-        <ImageGallery items={images} />
+    <div className='carrusel'>
+        <ImageGallery items={images}
+        showPlayButton={false}
+        showThumbnails={false}
+        showBullets={true} 
+        showNav={false}
+        autoPlay={true}
+        slideDuration={1000} />
     </div>
   )
 }
